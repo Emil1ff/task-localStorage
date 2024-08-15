44ba5c1795581ec556525a3c944daae4d9afc825
Emil1ff <emilhesenov398@gmail.com>
2024-08-15 17:40:24 +0400

    Update main.js

--- main.js
+++ main.js
@@ -1,60 +1,46 @@
-// Task 
-// 3 dene function quracaqsiz bir function usersleri adlandiran
-// function olacaq yeni dinamik olaraq usernameler vereceksiz
-// o functionda localeStoragede onu users keyine value olaraq
-// Array seklinde elave edecek
-// 2ci function dinamik olaraq username qebul edib localeStorageden
-// hemin usernameni silecek
-// 3 cu function ise umumi localestoragedeki users arrayini pars
-// olunmus halini logda daima gosderecek 
-// elave olaraq onu qeyd edimki localestorageye add olunan ve ya
-// silinende logda melumat verilsin bu haqda eger silinecek username yoxdu
-// error versin eger elave olaraq eyni username elave olunacaqsa ondada
-// error versin logda  ve elave edilmesin
-
-
-
 function addUser(username) {
-let users = JSON.parse(localStorage.getItem('users')) || [];
+    let users = JSON.parse(localStorage.getItem('users')) || [];
     try {
         if (users.includes(username)) {
-            throw new Error("username artiq var")
+            throw new Error("Username already exists");
         } else {
             users.push(username);
             localStorage.setItem('users', JSON.stringify(users));
-            console.log(`${username} elave edildi tebrikler`);
-            logUsers();
-        }
-    } catch (err) {
-        console.error(err.message)
-    }
-
-}
-function removeUser(username) {
-let users = JSON.parse(localStorage.getItem('users')) || [];
-    try {
-        if (!users.includes(username)) {
-            throw new Error(`${username} deye sexs qoqqu`);
-        } else {
-            users = users.filter(item => item != username);
-            localStorage.setItem('users', JSON.stringify(users)); 
-            console.log(`${username} silindi tebrikler`);
+            console.log(`${username} added successfully`);
             logUsers();
         }
     } catch (err) {
         console.error(err.message);
     }
 }
+
+function removeUser(username) {
+    let users = JSON.parse(localStorage.getItem('users')) || [];
+    try {
+        if (!users.includes(username)) {
+            throw new Error(`User ${username} does not exist`);
+        } else {
+            users = users.filter(item => item !== username);
+            localStorage.setItem('users', JSON.stringify(users));
+            console.log(`${username} removed successfully`);
+            logUsers(); 
+        }
+    } catch (err) {
+        console.error(err.message);
+    }
+}
+
 function logUsers() {
-    console.log(users);
+    let users = JSON.parse(localStorage.getItem('users')) || [];
+    console.log('Current users:', users);
 }
 
 addUser('Emil');
-addUser("Hemid")
+addUser('Hemid');
 addUser('Kenan');  
-addUser("Yusif")
-addUser('Kenan');
-
+addUser('Yusif');
+addUser('Kenan'); 
 
 removeUser('Emil');
-removeUser('Veli');
+removeUser('Veli'); 
+
